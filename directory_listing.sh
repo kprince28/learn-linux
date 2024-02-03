c571467359bf4c466001dd0c3ba84607c47122b6
Prince Kumar <pkumar@onemindservices.com>
2024-02-03 21:18:29 +0530

    added bash script to view all content in current directory

---
+++ directory_listing.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# A simple script to output the files in the current directory to the screen
+# Author: Prince Kumar
+# Date: Feb 03, 2034
+
+echo Your current directory is:
+pwd
+
+echo The files in your current directory are:
+ls
